3b87c55cf767763f3d80248bdcfdd460c61be1da
HiroKirigaya <privz286@gmail.com>
2022-04-03 22:54:34 -0300

    :robot: Add services

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from "uuid";
+
 import styles from "./Project.module.css";
 
 import { useParams } from "react-router-dom";
@@ -5,14 +7,16 @@ import { useState, useEffect } from "react";
 import Loading from "../layout/Loading";
 import Container from "../layout/Container";
 
-import Message from '../layout/Message';
+import Message from "../layout/Message";
 
 import ProjectForm from "../project/ProjectForm";
+import ServiceForm from "../service/ServiceForm";
 
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
   const [type, setType] = useState();
 
@@ -31,10 +35,11 @@ function Project() {
   }, [id]);
 
   function editPost(project) {
+    setMessage("");
     if (project.budget < project.cost) {
-      setMessage('The budget cannot be less than the cost of the project')
-      setType('error')
-      return false
+      setMessage("The budget cannot be less than the cost of the project");
+      setType("error");
+      return false;
     }
 
     fetch(`http://localhost:5000/projects/${project.id}`, {
@@ -46,10 +51,43 @@ function Project() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-          setProject(data)
-          setShowProjectForm(false)
-          setMessage('The project has changed with success')
-          setType('success')
+        setProject(data);
+        setShowProjectForm(false);
+        setMessage("The project has changed with success");
+        setType("success");
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function createService(project) {
+    setMessage('')
+    const lastService = project.services[project.services.length - 1];
+
+    lastService.id = uuidv4();
+
+    const lastServiceCost = lastService.cost;
+
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost);
+
+    if (newCost > parseFloat(project.budget)) {
+      setMessage("Exceeded budget, check the value of the service!");
+      setType("error");
+      project.services.pop();
+      return false;
+    }
+
+    project.cost = newCost;
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
       })
       .catch((err) => console.log(err));
   }
@@ -58,12 +96,16 @@ function Project() {
     setShowProjectForm(!showProjectForm);
   }
 
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
+  }
+
   return (
     <>
       {project.name ? (
         <div className={styles.projectDetails}>
           <Container customClass="column">
-              {message && <Message type={type} msg={message}/>}
+            {message && <Message type={type} msg={message} />}
             <div className={styles.detailsContainer}>
               <h1>Project: {project.name}</h1>
               <button onClick={toggleProjectForm} className={styles.btn}>
@@ -91,6 +133,25 @@ function Project() {
                 </div>
               )}
             </div>
+            <div className={styles.serviceFormContainer}>
+              <h2>Add a service:</h2>
+              <button onClick={toggleServiceForm} className={styles.btn}>
+                {!showServiceForm ? "Add service" : "Close"}
+              </button>
+              <div className={styles.projectInfo}>
+                {showServiceForm && (
+                  <ServiceForm
+                    handleSubmit={createService}
+                    btnText="Add Service"
+                    projectData={project}
+                  />
+                )}
+              </div>
+            </div>
+            <h2>Services</h2>
+            <Container customClass="start">
+              <p>Items services</p>
+            </Container>
           </Container>
         </div>
       ) : (
